b70928d73d036a249110c6e9288e2a798e57312f
Shiva Tiwari <oyeshivu@gmail.com>
2023-11-29 10:42:33 +0530

    swap program

---
+++ Python Assignment/swaping_4_values.py
@@ -0,0 +1,6 @@
+a =int(input("Enter the first value: "))
+b =int(input("Enter the first value: "))
+c =int(input("Enter the first value: "))
+d =int(input("Enter the first value: "))
+a, b, c, d = b, a, d, c
+print("Swapped values : ",a, b, c, d)
