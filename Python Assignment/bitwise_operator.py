6682fa9a4cff243426682f0832eb7f4f93b84693
Shiva Tiwari <oyeshivu@gmail.com>
2023-11-29 10:31:59 +0530

    bitwise and relational operator add

---
+++ Python Assignment/bitwise_operator.py
@@ -0,0 +1,6 @@
+x = int(input("Enter the value of x: "))
+y = int(input("Enter the value of y: "))
+print("x & y =", x & y)    
+print("x | y =", x | y)    
+print("~y =", ~ y)    
+print("x ^ y =", x ^ y)   
---
+++ Python Assignment/relational_operator.py
@@ -0,0 +1,9 @@
+a = 5
+b = 10
+
+print(a == b)  #return False
+print(a != b)  #return True
+print(a < b)   #return True
+print(a <= b)  #return True
+print(a > b)   #return False
+print(a >= b)  #return False
