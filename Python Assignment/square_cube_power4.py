570aad8a243bea93037a097e882ee9b99de8e0b7
Shiva Tiwari <oyeshivu@gmail.com>
2023-11-29 11:24:14 +0530

    square cube and power 4

---
+++ Python Assignment/square_cube_power4.py
@@ -0,0 +1,7 @@
+n = int(input("Enter the value: "))
+square = n ** 2
+cube = n ** 3
+power = n ** 4
+print("\n Square of the number ==>", square)
+print("\n Cube of the number ==>", cube)
+print("\n Power 4 of the number ==>", power)
