8626ce2950505abec5d3816817d397bdee77bc01
Shiva Tiwari <oyeshivu@gmail.com>
2023-12-07 14:24:35 +0530

    added some code

---
+++ name_checking.py
@@ -0,0 +1,6 @@
+names = ["shiva","suryansh","raj","Shipra","aman"]
+name = input("Enter the name to check\n")
+if name in names:
+    print("Your name is present in the list")
+else:
+    print("Your name is not in the list")
